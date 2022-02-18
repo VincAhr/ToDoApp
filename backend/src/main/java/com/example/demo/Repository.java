d66ebe7663349ff855042af9f40b762fecbf6374
Vincent Ahrens <vincent.ahrens@gmx.net>
2022-02-18 11:53:46 +0100

    Classes added

---
+++ backend/src/main/java/com/example/demo/Repository.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+@org.springframework.stereotype.Repository
+public class Repository {
+
+
+
+
+
+}
---
+++ backend/src/main/java/com/example/demo/Service.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+
+@org.springframework.stereotype.Service
+public class Service {
+
+
+    public void addtask (){
+
+    }
+
+    public void comletetask(){
+
+    }
+
+    public void deletetask
+
+
+
+
+
+}
