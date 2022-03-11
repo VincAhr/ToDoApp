f3908963dacdf61d3f03249c30824ed29e0ea65e
Vincent Ahrens <vincent.ahrens@gmx.net>
2022-03-11 16:01:44 +0100

    UUID

--- backend/src/main/java/de/neuefische/todo/Todo.java
+++ backend/src/main/java/de/neuefische/todo/Todo.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 public class Todo implements Comparable<Todo> {
-    private String id = "";
+    private String id = UUID.randomUUID().toString();
     private String task = "";
     private String description = "";
     private TodoStatus status = TodoStatus.Open;
